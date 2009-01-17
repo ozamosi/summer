6dc22431c46925bbbf428516c95669fa82c575de
Robin Sonefors <ozamosi@flukkost.nu>
2009-01-17 07:28:52 +0100

    Add test suite for torrent downloader

---
+++ libsummer/tests/test-download-torrent.c
@@ -0,0 +1,19 @@
+#include <glib.h>
+#include <libsummer/summer.h>
+
+static void
+init ()
+{
+	SummerDownload *dl;
+	dl = summer_create_download ("application/x-bittorrent", "http://localhost");
+	g_assert (SUMMER_IS_DOWNLOAD_TORRENT (dl));
+}
+
+int main (int argc, char *argv[]) {
+	g_type_init ();
+	g_thread_init (NULL);
+	g_test_init (&argc, &argv, NULL);
+	g_test_add_func ("/download-torrent/init", init);
+
+	return g_test_run ();
+}
