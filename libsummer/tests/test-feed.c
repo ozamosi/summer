3aff7c084f95561f517ddec3728d6184a138dcdb
Robin Sonefors <ozamosi@flukkost.nu>
2009-03-31 04:35:51 +0200

    Test invalid feeds

--- libsummer/tests/test-feed.c
+++ libsummer/tests/test-feed.c
@@ -29,6 +29,29 @@ create (WebFixture *fix, gconstpointer data)
 }
 
 static void
+on_invalid (SummerFeed *feed, gconstpointer data) {
+	GList *items = summer_feed_get_items (feed);
+	g_assert_cmpint (g_list_length (items), ==, 0);
+	g_main_loop_quit (loop);
+}
+
+static void
+invalid (WebFixture *fix, gconstpointer data)
+{
+	loop = g_main_loop_new (NULL, TRUE);
+	SummerFeed *feed;
+	feed = summer_feed_new ();
+	g_signal_connect (feed, "new-entries", G_CALLBACK (on_invalid), NULL);
+	gchar *url = g_strdup_printf ("http://localhost:%i/video/dummy_mp4", PORT);
+	summer_feed_start (feed, url);
+	g_free (url);
+	g_assert (feed != NULL);
+	g_object_unref (feed);
+	g_main_loop_run (loop);
+	g_main_loop_unref (loop);
+}
+
+static void
 settings ()
 {
 	summer_feed_set_default (g_get_tmp_dir (), 900);
@@ -56,6 +79,7 @@ int main (int argc, char *argv[]) {
 	g_test_init (&argc, &argv, NULL);
 
 	g_test_add ("/feed/create", WebFixture, 0, web_setup, create, web_teardown);
+	g_test_add ("/feed/invalid", WebFixture, 0, web_setup, invalid, web_teardown);
 	g_test_add ("/feed/settings", WebFixture, 0, web_setup, settings, web_teardown);
 	summer_shutdown ();
 
