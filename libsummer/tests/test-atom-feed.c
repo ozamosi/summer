7dd281526b5d5f1993ca45a39ddbe0b80525eea2
Robin Sonefors <ozamosi@flukkost.nu>
2009-04-26 21:32:39 +0200

    atom parser: Add test for non-web links

--- libsummer/tests/test-atom-feed.c
+++ libsummer/tests/test-atom-feed.c
@@ -18,14 +18,16 @@ parse ()
 	g_assert_cmpint (feed_data->updated, ==, time.tv_sec);
 	g_assert_cmpstr (feed_data->author, ==, "John Doe");
 
-	g_assert_cmpint (g_list_length (feed_data->items), ==, 1);
-	SummerItemData *item_data = SUMMER_ITEM_DATA (feed_data->items->data);
-	g_assert_cmpstr (item_data->title, ==, "Atom-Powered Robots Run Amok");
+	g_assert_cmpint (g_list_length (feed_data->items), ==, 2);
+	SummerItemData *item_data = SUMMER_ITEM_DATA (feed_data->items->next->data);
+	g_assert_cmpstr (summer_item_data_get_title (item_data), ==, "Atom-Powered Robots Run Amok");
 	g_assert_cmpstr (item_data->description, ==, "Some text.");
 	g_assert_cmpstr (item_data->id, ==, "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a");
 	g_assert_cmpstr (item_data->web_url, ==, "http://example.org/2003/12/13/atom03");
 	g_time_val_from_iso8601 ("2003-12-13T18:30:02Z", &time);	
 	g_assert_cmpint (item_data->updated, ==, time.tv_sec);
+	item_data = SUMMER_ITEM_DATA (feed_data->items->data);
+	g_assert_cmpstr (item_data->web_url, ==, NULL);
 	g_object_unref (feed_data);
 }
 
