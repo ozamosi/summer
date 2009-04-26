a0763e91bf8eb7208197a2c69e517c43e8ba77dd
Robin Sonefors <ozamosi@flukkost.nu>
2009-04-26 21:38:14 +0200

    feed: test more functions

--- libsummer/tests/test-feed.c
+++ libsummer/tests/test-feed.c
@@ -6,8 +6,14 @@ static GMainLoop *loop;
 
 static void
 on_new_entries (SummerFeed *feed, gconstpointer data) {
-	GList *items = summer_feed_get_items (feed);
+	GList *items = summer_feed_peek_items (feed);
 	g_assert_cmpint (g_list_length (items), ==, 20);
+	g_list_free (items);
+	items = summer_feed_get_items (feed);
+	g_assert_cmpint (g_list_length (items), ==, 20);
+	g_list_free (items);
+	items = summer_feed_get_items (feed);
+	g_assert_cmpint (g_list_length (items), ==, 0);
 	g_list_free (items);
 	g_main_loop_quit (loop);
 }
@@ -60,10 +66,9 @@ settings ()
 	gchar *orig_url = g_strdup_printf ("http://127.0.0.1:%i/feeds/epicfu",
 		PORT);
 	summer_feed_start (feed, orig_url);
-	gchar *cache_dir;
-	gchar *url;
-	gint frequency;
-	g_object_get (feed, "cache-dir", &cache_dir, "frequency", &frequency, "url", &url, NULL);
+	gchar *cache_dir = summer_feed_get_cache_dir (feed);
+	gint frequency = summer_feed_get_frequency (feed);
+	gchar *url = summer_feed_get_url (feed);
 	g_assert_cmpstr (cache_dir, ==, g_get_tmp_dir ());
 	g_assert_cmpint (frequency, ==, 900);
 	g_assert_cmpstr (url, ==, orig_url);
