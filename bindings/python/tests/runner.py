350c92ab3cdbe858dfb4de20e88401796aae5b9f
Robin Sonefors <ozamosi@flukkost.nu>
2009-05-13 19:20:05 +0200

    python-bindings: Add wrapper for download-error
    Since download-error emits a GError, which pygobject doesn't wrap,
    the dowload-error signal was useless. The new method on_error connects
    to the signal for the python object, and converts the result when it
    is triggered.
    
    Needs libpyglib, which does not have a pkg-config file, and is
    therefore linked to in a very hackish way.

--- bindings/python/tests/runner.py
+++ bindings/python/tests/runner.py
@@ -1,5 +1,9 @@
 import summer
 import unittest
+try:
+    import glib
+except ImportError: #mainloop was recently moved
+    import gobject as glib
 
 class TestDataTypes (unittest.TestCase):
     def testitem(self):
@@ -8,6 +12,32 @@ class TestDataTypes (unittest.TestCase):
         self.assertEqual (i['title'], 'hello world') #first
         # Make sure strings aren't freed
         self.assertEqual (i['title'], 'hello world') #second
+    def testonerror_noparam(self):
+        loop = glib.MainLoop ()
+        i = summer.ItemData ()
+        i.append_downloadable ('http://blubb')
+        dl = summer.create_download (i)
+        def error_cb (inner_dl, error):
+            self.assertEqual (inner_dl, dl)
+            self.assertEqual (error.message, "Download Failed")
+            self.assertEqual (inner_dl.get_property ('url'), 'http://blubb')
+            loop.quit ()
+        dl.on_error (error_cb)
+        dl.start ()
+        loop.run ()
+    def testonerror_param(self):
+        loop = glib.MainLoop ()
+        i = summer.ItemData ()
+        i.append_downloadable ('http://blubb')
+        dl = summer.create_download (i)
+        def error_cb (inner_dl, error, item):
+            self.assertEqual (inner_dl, dl)
+            self.assertEqual (error.message, "Download Failed")
+            self.assertEqual (inner_dl.get_property ('url'), 'http://blubb')
+            loop.quit ()
+        dl.on_error (error_cb, i)
+        dl.start ()
+        loop.run ()
 
 if __name__ == '__main__':
     unittest.main ()
